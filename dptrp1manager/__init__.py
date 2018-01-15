5a557e42f0ab78a2292466a0d42d0365aba2a5af
Christian Gross <christian.gross@mpq.mpg.de>
2018-01-15 21:55:12 +0100

    package starts to work

---
+++ dptrp1manager/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['DPManager', 'DPConfig', 'Synchronizer', 'Uploader', 'Downloader']
+from .base import DPManager
+from .base import DPConfig
+from .base import Synchronizer
+from .base import Uploader
+from .base import Downloader
