b9f901e4c7027953f30dc5427cc20c968df2c1c7
Christian Gross <christian.gross@mpq.mpg.de>
2018-04-22 06:45:17 +0200

    renaming

--- dptrp1manager/__init__.py
+++ dptrp1manager/__init__.py
@@ -1,6 +1,7 @@
+from .dptrp1manager import DPManager
+from .dptrp1manager import DPConfig
+from .dptrp1manager import Synchronizer
+from .dptrp1manager import Uploader
+from .dptrp1manager import Downloader
+
 __all__ = ['DPManager', 'DPConfig', 'Synchronizer', 'Uploader', 'Downloader']
-from .base import DPManager
-from .base import DPConfig
-from .base import Synchronizer
-from .base import Uploader
-from .base import Downloader
