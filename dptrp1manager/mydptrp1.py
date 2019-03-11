6d753ae61908784380410d54c05dc4f71845642a
Christian Gross <christian.gross@mpq.mpg.de>
2019-03-11 14:12:42 +0100

    fix get all function and limit it somehow

--- dptrp1manager/mydptrp1.py
+++ dptrp1manager/mydptrp1.py
@@ -64,8 +64,9 @@ class MyDigitalPaper(DigitalPaper):
         r = self._post_endpoint("/folders2", data=info)
 
     def list_all(self):
-        # data = self._get_endpoint('/documents2?entry_type=all').json()
-        data = self._get_endpoint('/documents2').json()
+        # data = self._get_endpoint("/documents2?entry_type=all").json()
+        data = self._get_endpoint("/documents2?entry_type=all&limit=1000").json()
+        # data = self._get_endpoint('/documents2').json()
         try:
             return data['entry_list']
         except KeyError:
